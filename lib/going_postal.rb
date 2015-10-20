643bd45aa4d2288ff6d10bb90967b906185d021f
Radu-Bogdan Croitoru <croitoruradubogdan@gmail.com>
2015-10-20 19:16:27 +0200

    Update RDoc info from lib file

--- lib/going_postal.rb
+++ lib/going_postal.rb
@@ -33,7 +33,7 @@
 # Currently supported countries are United Kingdom (GB, UK), United States
 # (US), Canada (CA), Australia (AU), New Zeland (NZ), South Africa (ZA), The
 # Netherlands (NL), Sweden (SE), Belgium (BE), Austria (AT), Norway (NO),
-# Germany (DE), Luxembourg (LU), Denmark (DE) and France (FR).
+# Germany (DE), Luxembourg (LU), Denmark (DE), France (FR) and Romania(RO).
 # 
 # Ireland (IE) as well as 60+ other countries that do not use postcodes
 # - see GoingPostal::COUNTRIES_WITHOUT_POSTCODES - are supported insomuch as,
